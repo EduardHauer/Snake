19b51d8a68ab42a93909296654dc41f28059d970
EduardHauer <eduard.hauer.genadiy@gmail.com>
2017-11-01 16:18:03 +0100

    adding Drawing Picture

---
+++ SnakeGame/SnakeGame/Painting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Painting
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char objChar;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a">It's X of symbol</param>
+        /// <param name="b">It's Y of symbol</param>
+        /// <param name="ch">It's Letter/Nmber/... of symbol</param>
+        public Point(int a, int b, char ch)
+        {
+            x = a;
+            y = b;
+            objChar = ch;
+        }
+
+        /// <summary>
+        /// Paint your point
+        /// </summary>
+        public void Paint()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(objChar);
+        }
+    }
+}
--- SnakeGame/SnakeGame/Program.cs
+++ SnakeGame/SnakeGame/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Painting;
 
 namespace SnakeGame
 {
@@ -9,7 +10,6 @@ namespace SnakeGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
         }
     }
 }
